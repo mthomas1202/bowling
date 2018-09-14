cc49f6b4a45e6352053f8e70b4ccaf5383f8bdde
Matthew Thomas <matt.thomas1202@yahoo.com>
2018-09-14 13:58:52 -0400

    camera and basic ball logic

---
+++ Assets/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public GameObject bowlingBall;
+
+    public float ballDistance = 2.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        bowlingBall.transform.position = transform.position + transform.forward * ballDistance;
+	}
+}
