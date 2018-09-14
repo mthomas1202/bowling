0743be97bd224847b616730e342ee3a5afe92e49
Matthew Thomas <matt.thomas1202@yahoo.com>
2018-09-14 15:06:26 -0400

    basic lane and collision logic/design

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour {
     public GameObject bowlingBall;
 
     public float ballDistance = 2.5f;
+    public float ballThrowingForce = 200f;
+    private bool holding = true;
 
 	// Use this for initialization
 	void Start () {
@@ -15,7 +17,17 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        
+        if (holding)
+        {
+            bowlingBall.transform.position = transform.position + transform.forward * ballDistance;
 
-        bowlingBall.transform.position = transform.position + transform.forward * ballDistance;
+            if (Input.GetKeyDown("space") || Input.GetMouseButtonDown(0))
+            {
+                holding = false;
+                bowlingBall.GetComponent<Rigidbody>().useGravity = true;
+                bowlingBall.GetComponent<Rigidbody>().AddForce(transform.forward * ballThrowingForce);
+            }
+        }
 	}
 }
